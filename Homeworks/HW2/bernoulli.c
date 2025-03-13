66ed68383bec49fd4b8f87d4f92691efdf1f56b1
Vanja Stojanović <vanja.stojanovic25@gmail.com>
2025-03-13 23:53:00 +0100

    Bernoulli done

--- Homeworks/HW2/bernoulli.c
+++ Homeworks/HW2/bernoulli.c
@@ -3,23 +3,51 @@
 #include "lib/tests.h"
 #include "lib/utils.h"
 
+int write_training_data() {
+  FILE* file = fopen("./lib/ode.data", "w");
+  if(!file) {
+    perror("Error opening file");
+    return 1;  // Exit the program or handle the error
+  }
+
+  Points2D results = RK4(fP, fQ, 2, 2, -1, 3, 0.0001);
+
+  fprintf(file, "%d %d %d\n", 10001, 1, 1);
+
+  for(size_t i = 0; i < 10001; i++) {
+    fprintf(file, "%-7.6f %-7.6f\n", results.x[i], results.y[i]);
+  }
+
+  fclose(file);
+  return 0;
+}
+
 void train_neural_network() {
+  printf("Generating training data...\n");
+  write_training_data();
+
   const unsigned int num_input = 1;
   const unsigned int num_output = 1;
-  const unsigned int num_layers = 4;
+  const unsigned int num_layers = 5;
   const unsigned int num_neurons_hidden1 = 10;
-  const unsigned int num_neurons_hidden2 = 6;
-  const float desired_error = 0.00001;
+  const unsigned int num_neurons_hidden2 = 10;
+  const unsigned int num_neurons_hidden3 = 8;
+  const float desired_error = 0.000001;
   const unsigned int max_epochs = 500000;
   const unsigned int epochs_between_reports = 1000;
 
   struct fann* ann = fann_create_standard(num_layers, num_input, num_neurons_hidden1,
-                                          num_neurons_hidden2, num_output);
+                                          num_neurons_hidden2, num_neurons_hidden3, num_output);
 
   fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
   fann_set_activation_function_output(ann, FANN_LINEAR);
 
+  // Optimizations (After adding a third hidden layer, the NN converged (with given error) in 5274
+  // epochs)
   fann_set_learning_rate(ann, 0.1);
+  fann_set_learning_momentum(ann, 0.8);
+  fann_set_training_algorithm(ann, FANN_TRAIN_RPROP);  // RPROP = Resilient propagation
+  fann_set_train_error_function(ann, FANN_ERRORFUNC_TANH);
 
   printf("Training network...\n");
 
@@ -32,29 +60,31 @@ void train_neural_network() {
   fann_destroy(ann);
 }
 
-int inference_from_nn(FILE* net_file) {
+int inference_from_nn() {
   struct fann* ann = fann_create_from_file("ode_solver.net");
   if(!ann) {
     printf("Error: Could not load the neural network\n");
     return 1;
   }
 
-  double x_values[] = {0.0, 0.5, 1.0, 1.5, 2.0, 2.5};
-  int num_points = 6;
+  double x_values[] = {3.0, 3.01, 3.02, 3.03, 3.04, 3.05, 3.06};
 
-  printf("Inference results for dy/dx = x^2*y^2 - x*y^2:\n");
+  printf("Inference results for dy/dx = x^3*y^2 - (4/x)*y^2:\n");
   printf("------------------------------------------------\n");
-  printf("   x   |       y       \n");
-  printf("-------+---------------\n");
+  printf("   x   |       y       |      RK4 y     \n");
+  printf("-------+---------------+----------------\n");
 
-  for(int i = 0; i < num_points; i++) {
+  double rk_results[] = {-0.026382, -0.025849, -0.025330, -0.024824,
+                         -0.024332, -0.023851, -0.023383};
+
+  for(int i = 0; i < 7; i++) {
     fann_type input = x_values[i];
 
     // Inference
     fann_type* output = fann_run(ann, &input);
 
     // Output the predicted y value
-    printf(" %.2f  | %.10f\n", x_values[i], output[0]);
+    printf(" %-5.2f | %-13.10f | %-12.10f\n", x_values[i], output[0], rk_results[i]);
   }
 
   fann_destroy(ann);
@@ -62,12 +92,14 @@ int inference_from_nn(FILE* net_file) {
   return 0;
 }
 
-int main(int argc, char const** args) {
-  FILE* file = fopen("./ode_solver.net", "r");
+int main() {
+  FILE* file = fopen("./lib/ode.data", "r");
   if(file) {
-    return inference_from_nn(file);
+    fclose(file);
+    return inference_from_nn();
   } else {
+    fclose(file);
     train_neural_network();
+    return 0;
   }
-  return 0;
 }
--- Homeworks/HW2/lib/tests.c
+++ Homeworks/HW2/lib/tests.c
@@ -1,37 +1,47 @@
 #include "tests.h"
 
 // Example functions:
-// P(x) = x
-// Q(x) = x^2
+// P(x) = 4/x
+// Q(x) = x^3
 // n = 2
-double fP(double x) { return x; }
+// y(2) = -1
+// x > 0
 
-double fQ(double x) { return x * x; }
+double fP(double x) { return 4 / x; }
+
+double fQ(double x) { return x * x * x; }
 
 void testRK() {
   const double TOLERANCE = 1e-5;
 
-  Points2D results = RK4(fP, fQ, 2.0f, 0, 2, 1, 0.25f);
+  Points2D results = RK4(fP, fQ, 2, 2, -1, 3, 0.1);
   printf("Testing RK4 function...");
-  printf("\tP(x) = 4/sin(x)\n\tQ(x) = x^2\n\tn = 2\n\tx_0 = 0\n\ty_0=2\n\th = 0.25\n");
+  printf("\tP(x) = 4/x\n\tQ(x) = x^3\n\tn = 2\n\tx_0 = -1\n\ty_0=2\n\th = 0.1\n");
 
   printf("Expected: \n");
-  printf("| %-4s | %-5s | %-7s |\n", "step", "x", "y");
-  printf("|------|-------|---------|\n");
-  printf("| %-4d | %-5.2f | %-7.5f |\n", 0, 0.00, 2.00000);
-  printf("| %-4d | %-5.2f | %-7.5f |\n", 1, 0.25, 1.95846);
-  printf("| %-4d | %-5.2f | %-7.5f |\n", 2, 0.50, 1.91292);
-  printf("| %-4d | %-5.2f | %-7.5f |\n", 3, 0.75, 1.98175);
-  printf("| %-4d | %-5.2f | %-7.5f |\n", 4, 1.00, 2.41706);
-  double x[] = {0.00, 0.25, 0.50, 0.75, 1.00};
-  double y[] = {2.00000, 1.95846, 1.91292, 1.98175, 2.41706};
+  printf("| %-4s | %-5s | %-8s |\n", "step", "x", "y");
+  printf("|------|-------|----------|\n");
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 0, 2.0, -1.00000);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 1, 2.1, -0.45513);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 2, 2.2, -0.26784);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 3, 2.3, -0.17537);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 4, 2.4, -0.12238);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 5, 2.5, -0.08917);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 6, 2.6, -0.06706);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 7, 2.7, -0.05169);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 8, 2.8, -0.04063);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 9, 2.9, -0.03247);
+  printf("| %-4d | %-5.2f | %-7.5f |\n", 10, 3.0, -0.02630);
+  double x[] = {2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0};
+  double y[] = {-1.00000, -0.45513, -0.26784, -0.17537, -0.12238, -0.08917,
+                -0.06706, -0.05169, -0.04063, -0.03247, -0.02630};
   Points2D expected = (Points2D){.x = x, .y = y};
 
   printf("Computed: \n");
-  printf("| %-4s | %-5s | %-7s |\n", "step", "x", "y");
-  printf("|------|-------|---------|\n");
-  for(size_t i = 0; i < 5; i++) {
-    printf("| %-4zu | %-5.2f | %-7.5f |\n", i, results.x[i], results.y[i]);
+  printf("| %-4s | %-5s | %-8s |\n", "step", "x", "y");
+  printf("|------|-------|----------|\n");
+  for(size_t i = 0; i < 11; i++) {
+    printf("| %-4zu | %-5.2f | %-8.5f |\n", i, results.x[i], results.y[i]);
     assert(approx_equal(expected.x[i], results.x[i], TOLERANCE));
     assert(approx_equal(expected.y[i], results.y[i], TOLERANCE));
   }
